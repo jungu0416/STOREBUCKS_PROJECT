a8b53e484f6da56e82214d17f60c3112f049df16
jungu0416 <jungu0416@naver.com>
2021-08-17 19:51:58 +0900

    storbucks

---
+++ StorbucksProject/src/com/admin/www/Connect.java
@@ -0,0 +1,8 @@
+package com.admin.www;
+
+public class Connect {
+	public static String driver = "oracle.jdbc.driver.OracleDriver";
+	public static String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	public static String id = "project";
+	public static String pw = "project";
+}
